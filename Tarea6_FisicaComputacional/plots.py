31bdbf8b13d39c7a90ae28a2ea10702bcb1aec40
Miguel Aldana Páez <miguel09@ubuntu.(none)>
2013-10-29 11:46:01 -0500

    Graficas de posiciones de las particulas

--- Tarea6_FisicaComputacional/plots.py
+++ Tarea6_FisicaComputacional/plots.py
@@ -1,10 +1,15 @@
 import matplotlib as plt
 
 """
-   Modulo que realiza las graficas de posiciones  para las particulas.
+   Modulo que realiza las graficas de posiciones para las particulas para observar la evolucion de estas en el tiempo.
 
    Input:
-   - Arhivos de entrada que contienen velocidades y posiciones de las particulas
+   - Archivo_1: archivo con condiciones iniciales de posicion y velocidad de las particulas
+   - Archivo_2 = posiciones y velocidades de las particulas en los proximos 5 mil millones de anos.
+   - Archivo_3 = posiciones y velocidades de las particulas en los proximos 10 mil millones de anos.
+   - Archivo_4 = posiciones y velocidades de las particulas en los proximos 15 mil millones de anos.
+   - Archivo_5 = posiciones y velocidades de las particulas en los proximos 20 mil millones de anos.
+   - Archivo_6 = posiciones y velocidades de las particulas en los proximos 25 mil millones de anos.
 
    Output:
    - Graficas correspondientes a cada archivo
