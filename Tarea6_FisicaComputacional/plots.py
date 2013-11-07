a56d8c751ea062b1feffd53f233072aaf77c133d
Miguel Aldana Páez <miguel09@ubuntu.(none)>
2013-11-07 12:05:21 -0500

    Graficadora

--- Tarea6_FisicaComputacional/plots.py
+++ Tarea6_FisicaComputacional/plots.py
@@ -1,4 +1,8 @@
-import matplotlib as plt
+﻿#!/usr/bin/python
+
+import matplotlib.pyplot as plt
+import numpy as np
+from matplotlib import pyplot
 
 """
    Modulo que realiza las graficas de posiciones para las particulas para observar la evolucion de estas en el tiempo.
@@ -15,5 +19,50 @@ import matplotlib as plt
    - Graficas correspondientes a cada archivo
 """
 
-def plotting(x,y):
-    plt.plot(x,y)
+data_0 = np.loadtxt("Condiciones_iniciales.dat")
+
+pyplot.plot(data_0[:,1],data_0[:,2], 'ko')
+
+pyplot.show()
+
+data_0_2 = np.loadtxt("Condiciones_iniciales_2.dat")
+
+pyplot.plot(data_0_2[:,1],data_0_2[:,2], 'ko')
+
+pyplot.show()
+
+data_0_3 = np.loadtxt("Condiciones_iniciales_3.dat")
+
+pyplot.plot(data_0_3[:,1],data_0_3[:,2], 'ko')
+
+pyplot.show()
+
+data_1 = np.loadtxt("Datos_evolucion_1.dat")
+
+pyplot.plot(data_1[:,0],data_1[:,1], 'ko')
+
+pyplot.show()
+
+data_2 = np.loadtxt("Datos_evolucion_2.dat")
+
+pyplot.plot(data_2[:,0],data_2[:,1], 'ko')
+
+pyplot.show()
+
+data_3 = np.loadtxt("Datos_evolucion_3.dat")
+
+pyplot.plot(data_3[:,0],data_3[:,1], 'ko')
+
+pyplot.show()
+
+data_4 = np.loadtxt("Datos_evolucion_4.dat")
+
+pyplot.plot(data_4[:,0],data_4[:,1], 'ko')
+
+pyplot.show()
+
+data_5 = np.loadtxt("Datos_evolucion_5.dat")
+
+pyplot.plot(data_5[:,0],data_5[:,1], 'ko')
+
+pyplot.show()
